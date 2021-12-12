f0de6b4405f686933e4c2652f484537d5ab64d88
MSandwidi <13341572+msandwidi@users.noreply.github.com>
2021-12-12 10:03:40 -0500

    Update FlutterMoneyFormatterPlugin.java

--- android/src/main/java/com/octapush/moneyformatter/fluttermoneyformatter/FlutterMoneyFormatterPlugin.java
+++ android/src/main/java/com/octapush/moneyformatter/fluttermoneyformatter/FlutterMoneyFormatterPlugin.java
@@ -1,5 +1,8 @@
 package com.octapush.moneyformatter.fluttermoneyformatter;
 
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -7,12 +10,24 @@ import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** FlutterMoneyFormatterPlugin */
-public class FlutterMoneyFormatterPlugin implements MethodCallHandler {
+public class FlutterMoneyFormatterPlugin implements FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private MethodChannel channel;
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "hello");
+    channel.setMethodCallHandler(this);
+  }
+ 
   /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
+  /*public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_money_formatter");
     channel.setMethodCallHandler(new FlutterMoneyFormatterPlugin());
-  }
+  }*/
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
@@ -21,5 +36,10 @@ public class FlutterMoneyFormatterPlugin implements MethodCallHandler {
     } else {
       result.notImplemented();
     }
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
   }
 }
